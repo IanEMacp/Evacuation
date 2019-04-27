6c01aa559b06d61f69507bef2f1ee1b9856e3d08
mtb18 <48257955+mtb18@users.noreply.github.com>
2019-04-27 13:04:55 -0400

    finish draft 1
    currently able to run 1 simulation at a time, with randomized threat and actions. not cute, but functions.

--- Translation.py
+++ Translation.py
@@ -10,12 +10,11 @@ def chance():
 def emergency(emergency_chance):
     x = chance()
     if x>emergency_chance:
-        return false
-    return true
+        return False
+    return True
 
 #in the paper nature is a lowercase omega, and accuracy is tau
-def alarmCall(emergency_chance, accuracy):
-    nature = emergency(emergency_chance)
+def alarmCall(nature, accuracy):
     x = chance()
     if x < accuracy:
         return nature
@@ -23,19 +22,20 @@ def alarmCall(emergency_chance, accuracy):
 
 #this is wrong. I know that to be true, but I can't figure out what
 #the paper actually meant.
+#it's not actually used anywhere so /shrug
 def authorityBelief(prior, signal_accuracy):
-    return (prior*signal_accuracy)/(prior*signal_accuracy+(1-prior)(1-signal_accuracy))
+    return (prior*signal_accuracy)/(prior*signal_accuracy+(1-prior)*(1-signal_accuracy))
 
 #alarm is X and accuracy is tau
 def authorityDecision(alarm, alarm_accurate, stratN, stratT, prior):
     probability_order = stratN * ((1 - prior) * alarm_accurate + prior * (1 - alarm_accurate)) + stratT * ((1 - prior) * (1 - alarm_accurate) + prior * alarm_accurate)
-    if chance() < probabilityOrder:
-        return true
-    return false
+    if chance() < probability_order:
+        return True
+    return False
 
 def evacuee_belief(aAction, pOrder, alarm_accurate, prior):
-    pAlarm = (1 - prior)(1 - alarm_accurate) + prior * alarm_accurate
-    aBelief = authority_belief(prior, alarm_accurate)
+    pAlarm = (1 - prior)*(1 - alarm_accurate) + prior * alarm_accurate
+    aBelief = authorityBelief(prior, alarm_accurate)
     if pOrder > pAlarm:
         return (1 - aBelief) + (prior * (1 - prior) * (2 * alarm_accurate - 1)) / (( 1 - pAlarm) * pOrder)
     return aBelief
@@ -43,13 +43,17 @@ def evacuee_belief(aAction, pOrder, alarm_accurate, prior):
 #the evacuee's strategy is just how likely they are to actually believe
 #the authority when it calls the alarm.
 def evacuee_decision(aAction, pOrder, alarm_accurate, prior):
-    if aAction and chance() < EvacueeBelief(aAction, pOrder, alarm_accurate, prior):
-            return true
-    return false
+    if aAction and chance() < evacuee_belief(aAction, pOrder, alarm_accurate, prior):
+            return True
+    return False
 
 #these payoffs have so many parameters... save me
 #how are we going to learn anything from this?
 def authority_payoff(state, aAction, eAction):
+    risk = 0.25
+    deadDependent = 5
+    lostWork = 3
+    serviceFee = 1
     if state:
         if aAction:
             if eAction:
@@ -63,6 +67,9 @@ def authority_payoff(state, aAction, eAction):
     return 0
 
 def evacuee_payoff(state, eAction):
+    risk = .25
+    death = 10
+    exitCost = 2
     if state:
         if eAction:
             return 0 - risk * death - exitCost
@@ -70,5 +77,25 @@ def evacuee_payoff(state, eAction):
     if eAction:
         return 0 - exitCost
     return 0
-            
+
+def main():
+    eChance = .4
+    nature = emergency(eChance)
+    accuracy = .9
+    alarm = alarmCall(nature, accuracy)
+    strategyOrder = .5
+    strategyDont = .5
+    if chance() < .3:
+        prior = True
+    else:
+        prior = False
+    order = authorityDecision(alarm, accuracy, strategyDont, strategyOrder, prior)
+    leave = evacuee_decision(order, strategyDont, accuracy, prior) #not sure if strategyDont is actually what I want there
+    print("Threat: " + str(nature))
+    print("Evacuation Order: " + str(order))
+    print("Exit Attempt: " + str(leave))
+    print("Evacuee Payoff: " + str(evacuee_payoff(nature, leave)))
+    print("Authority Payoff: " + str(authority_payoff(nature, order, leave)))
+
+main()
             
